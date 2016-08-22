8b644240352414368b15c99dda8fecd0dfb6b35a
Adrian Mlodzianowski <amlodzianowski@us.logicworks.net>
2016-08-22 08:47:49 -0400

    adding appspec skeletons

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,5 @@ from sc import generator
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    return generator("southpark","10.252.1.40")
+    result = generator("southpark","10.252.1.40")
+    return result
