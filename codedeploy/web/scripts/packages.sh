6e9df5510497e1bee58af6ea2f5959f2bf8f3cef
Adrian Mlodzianowski <amlodzianowski@us.logicworks.net>
2016-08-22 16:19:38 -0400

    changing files to CRLF

--- codedeploy/web/scripts/packages.sh
+++ codedeploy/web/scripts/packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get install -y nginx-full uwsgi uwsgi-plugin-python
-rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
 mkdir /data
--- scripts/packages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt-get install -y nginx-full uwsgi uwsgi-plugin-python
-rm /etc/nginx/sites-enabled/default
-mkdir /data
