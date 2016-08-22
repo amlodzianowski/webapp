da03257439ed573905e01f52488d0535b5f11fde
Adrian Mlodzianowski <amlodzianowski@us.logicworks.net>
2016-08-22 18:34:39 -0400

    fixing syntax

--- codedeploy/web/scripts/packages.sh
+++ codedeploy/web/scripts/packages.sh
@@ -2,12 +2,12 @@
 apt-get install -y nginx-full uwsgi uwsgi-plugin-python
 
 #if file exists, remove it
-if [ -f "/etc/nginx/sites-available/default"]
+if [ -f "/etc/nginx/sites-available/default" ]
 then
   rm /etc/nginx/sites-available/default
 fi
 #if /data directory does not exist, create it
-if [ ! -d "/data"]
+if [ ! -d "/data" ]
 then
   mkdir /data
 fi
--- codedeploy/web/scripts/start.sh
+++ codedeploy/web/scripts/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 SERVICE=nginx;
-ln -s /etc/nginx/sites-available/sites-default /etc/nginx/sites-enabled/sites-default
 
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
 then
