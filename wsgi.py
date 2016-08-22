2b1c9332aa31ad26e122dcf713276e40ca87a122
Adrian Mlodzianowski <amlodzianowski@us.logicworks.net>
2016-08-22 19:36:05 -0400

    adding stuff to app tier

--- wsgi.py
+++
@@ -1,6 +0,0 @@
-from sc import generator
-
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    result = generator("southpark","10.252.1.40")
-    return result
