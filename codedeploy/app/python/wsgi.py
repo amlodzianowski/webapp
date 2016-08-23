55a52883b20678246fa4cfe8ff3a48e3a7006e2f
Adrian Mlodzianowski <amlodzianowski@us.logicworks.net>
2016-08-23 16:09:39 -0400

    swapping mongo ip for dns name

--- codedeploy/app/python/wsgi.py
+++ codedeploy/app/python/wsgi.py
@@ -4,5 +4,5 @@ from generator import generator
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    result = generator("southpark","10.252.1.40")
+    result = generator("southpark","mongo.adrian-training.local")
     return result
