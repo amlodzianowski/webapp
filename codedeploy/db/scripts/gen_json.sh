915d4cab8209c2cabc202647cbe748fd16fac3ed
Adrian Mlodzianowski <amlodzianowski@us.logicworks.net>
2016-08-25 12:55:01 -0400

    adding chmod to afterinstall for mongo

--- codedeploy/db/scripts/gen_json.sh
+++ codedeploy/db/scripts/gen_json.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+/bin/chmod ugo+x /data/mongo-script.sh
 /data/mongo-script.sh /data/sp-charlist.txt > /data/sp-charlist.json
 /data/mongo-script.sh /data/sw-charlist.txt > /data/sw-charlist.json
