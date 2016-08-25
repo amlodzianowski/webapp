63d149e3233c4e5d7631a50d6eba41bdffe38967
Adrian Mlodzianowski <amlodzianowski@us.logicworks.net>
2016-08-25 13:07:05 -0400

    fixing crlf issue and naming in start.sh

--- codedeploy/db/scripts/start.sh
+++ codedeploy/db/scripts/start.sh
@@ -3,11 +3,11 @@ SERVICE=mongodb;
 DBNAME=chars
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
 then
-  service nginx mongodb
+  service $SERVICE restart
 else
-  service nginx mongodb
+  service $SERVICE start
 fi
 
 /usr/bin/mongo $DBNAME --eval "db.dropDatabase()"
-/usr/bin/mongoimport --db chars --collection southpark < /data/sp-chars.json
-/usr/bin/mongoimport --db chars --collection starwars < /data/sw-chars.json
+/usr/bin/mongoimport --db chars --collection southpark < /data/sp-charlist.json
+/usr/bin/mongoimport --db chars --collection starwars < /data/sw-charlist.json
