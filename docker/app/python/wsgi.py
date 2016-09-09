4fde8d7e49c12d43f1b3cb8190facdaf595a458f
Adrian Mlodzianowski <amlodzianowski@us.logicworks.net>
2016-09-09 16:15:37 -0400

    adding files to app and db

---
+++ docker/app/python/wsgi.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("/data")
+from generator import generator
+
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    result = generator("starwars","mongo.adrian-training.local")
+    return result
