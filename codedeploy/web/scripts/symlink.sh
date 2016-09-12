e961313411c0e1d75f22e06c8bae837a58e1d045
Adrian Mlodzianowski <amlodzianowski@us.logicworks.net>
2016-09-12 13:39:49 -0400

    adding hostname functionality to codedpeloy web

--- codedeploy/web/scripts/symlink.sh
+++ codedeploy/web/scripts/symlink.sh
@@ -5,3 +5,6 @@ if [ ! -h "/etc/nginx/sites-enabled/sites-default" ]
 then
   ln -s /etc/nginx/sites-available/sites-default /etc/nginx/sites-enabled/sites-default
 fi
+
+#Echo server's hostname into a file
+/bin/echo "Hostname: $(/bin/hostname -f)" > /data/hostname
