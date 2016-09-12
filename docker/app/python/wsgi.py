8609aefdfc4f26a7a7a2f3b991806b0c5620b083
Adrian Mlodzianowski <amlodzianowski@us.logicworks.net>
2016-09-12 19:28:41 -0400

    testing env variables

--- docker/app/python/wsgi.py
+++ docker/app/python/wsgi.py
@@ -4,5 +4,5 @@ from generator import generator
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    result = generator("starwars","mongo.adrian-training.local", 25000)
+    result = generator("starwars",os.environ['UPSTREAM_HOST'], os.environ['UPSTREAM_PORT'])
     return result
