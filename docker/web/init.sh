6dec76987f3354ffd52fc56dc942a6c0cee88221
Adrian Mlodzianowski <amlodzianowski@us.logicworks.net>
2016-09-13 08:34:46 -0400

    removing nginx start from init

--- docker/web/init.sh
+++ docker/web/init.sh
@@ -2,4 +2,3 @@
 /bin/sed -i "s/serverplaceholder/${UPSTREAM_HOST}/" /etc/nginx/sites-available/sites-default
 /bin/sed -i "s/portplaceholder/${UPSTREAM_PORT}/" /etc/nginx/sites-available/sites-default
 /bin/echo "Hostname: $(/bin/hostname -f)" > /data/hostname;
-/usr/sbin/nginx -g 'daemon off
